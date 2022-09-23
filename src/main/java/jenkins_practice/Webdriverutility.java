87ce4ae87bc39c1c3ee20dcbe4dd61d5ccfe856e
PREM KUMAR R <PREM KUMAR R@DESKTOP-VUHVFPU>
2022-09-23 15:02:58 +0530

    new commit

---
+++ src/main/java/jenkins_practice/Webdriverutility.java
@@ -0,0 +1,82 @@
+package jenkins_practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Webdriverutility {
+	private WebDriver driver;
+	public WebDriver openBrowser(String browser) 
+	{
+		
+		switch (browser) {
+		case "firefox":WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+			break;
+		case "chrome":WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		break;
+		default:System.out.println("Please Enter Valid Browser Key");
+			break;
+		}
+		return driver;
+	}
+	
+	/**
+	 * This method is to maximize the browser
+	 * @param driver
+	 */
+	public void maximizeBrowser(WebDriver driver) {
+		driver.manage().window().maximize();
+	}
+	
+	/**
+	 * This method is use to wait till page gets load.
+	 * @param driver
+	 * @param time
+	 */
+	public void pageWaitTillLoad(WebDriver driver, long time)
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
+	}
+	
+	/**
+	 * this method is used to navigate to the application by passing url
+	 * @param driver
+	 * @param url
+	 */
+	public void navigateToApplication(WebDriver driver, String url)
+	{
+		driver.get(url);
+	}
+	
+	/**
+	 * This  method is used to close the application.
+	 * @param driver
+	 */
+	public void closeTheApplication(WebDriver driver)
+	{
+		driver.quit();
+	}
+	/**
+	 * This method is used to launch the browser, maximize, wait till the page gets load, then open application with the help of url.
+	 * @param browser
+	 * @param time
+	 * @param url
+	 * @return
+	 */
+	public WebDriver openBrowserWithApplication(String browser,long time,String url)
+	{
+		WebDriver driver = openBrowser(browser);
+		maximizeBrowser(driver);
+		pageWaitTillLoad(driver,time);
+		navigateToApplication(driver,url);
+		return driver;
+		
+	}
+
+}
--- src/test/java/jenkins_trial/Practice3Test.java
+++ src/test/java/jenkins_trial/Practice3Test.java
@@ -1,6 +1,9 @@
 package jenkins_trial;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import jenkins_practice.Webdriverutility;
 
 public class Practice3Test {
 
@@ -11,5 +14,6 @@ public class Practice3Test {
 		String url = System.getProperty("ul");
 		System.out.println(browser);
 		System.out.println(url);
+		WebDriver driver = new Webdriverutility().openBrowserWithApplication(browser, 15, url);
 	}
 }
